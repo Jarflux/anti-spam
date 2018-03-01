a8675d208f1e5732b2ca44b98214598f63c1d1f6
Jarflux <ben.oeyen@gmail.com>
2018-03-01 17:22:10 +0100

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	if(window.location.href.indexOf('confluence') !== -1){
+		var e = document.getElementById("notifyWatchers");
+		if(e){
+        	e.checked = false;
+      	}
+	}
+}
