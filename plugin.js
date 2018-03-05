9305c72eb93b009efe15206fb72f225c385d47d5
Jarflux <ben.oeyen@gmail.com>
2018-03-05 15:25:52 +0100

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	if(window.location.href.indexOf('confluence') !== -1){
 		var e = document.getElementById("notifyWatchers");
 		if(e){
-        	e.checked = false;
-      	}
+        		e.checked = false;
+      		}
 	}
 }
