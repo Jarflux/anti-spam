d3a58bffc81247074f6dc1034c489b6e985010b0
Jarflux <ben.oeyen@gmail.com>
2018-03-05 19:21:51 +0100

    Delete plugin.js

--- plugin.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-	if(window.location.href.indexOf('confluence') !== -1){
-		var e = document.getElementById("notifyWatchers");
-		if(e){
-        		e.checked = false;
-      		}
-	}
-}
